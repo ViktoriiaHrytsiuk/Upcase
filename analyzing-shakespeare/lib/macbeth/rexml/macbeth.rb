b63f07f51b19c0586ade13a94d3a4683d0030b7a
viktoria.hrytsiuk <grytsykvika@gmail.com>
2018-11-30 12:32:43 +0200

    change macbeth class

--- analyzing-shakespeare/lib/macbeth/rexml/macbeth.rb
+++ analyzing-shakespeare/lib/macbeth/rexml/macbeth.rb
@@ -48,81 +48,58 @@ class Macbeth < BaseParser
   end
 
   def act_iterator
-    result = {}
+    result = []
     act_elements do |act_element|
-      act = Act.new(act_element: act_element)
-      title = act.xml_block("TITLE")
-
-      scene_block = {}
-      act.xml_block("SCENE") do |scene|
-        grouped_elements = scene.elements.group_by(&:name)
-        scene_name = grouped_elements["TITLE"].first.text
-        speech_block = grouped_elements["SPEECH"]
-        scene_block[scene_name] = speech_block
-      end
-      scene_block
-      result[title] = scene_block
+      act = Act.new(:act_element => act_element)
+      act.xml_block("TITLE")
+      act.speech
+      result << act
     end
-    result
+    act_object(result)
   end
 
-  # def max_act_speech
-  #   scene_speeches = {}
-  #   scene_elements do |scene_element|
-  #     scene = Scene.new(scene_element: scene_element)
-  #     scene_title = scene.xml_block("TITLE")
-  #     scene_speeches[scene_title] = scene.longest_speech
-  #   end
-  #   scene_speeches
+  def act_object(result)
+    act_result = result.map(&:speech)
+    speech = act_result.map do |each_hash|
+      each_hash.values.flatten
+    end.max_by(&:last)
 
-  #   act_speech_title = {}
-  #   act_speech = scene_speeches.values.max_by(&:last)
-  #   scene_title = scene_speeches.key(act_speech)
-  #   act_speech_title[scene_title] = act_speech
-  # end
+    scene_title = act_result.map { |each_hash| each_hash.key(speech) }.compact[0]
 
-  # def act_object(result)
-  #   act_result = result.map(&:speech)
+    act_title = nil
+    result.map do |act|
+      act_title = act.xml_block("TITLE") if act.speech.include?(scene_title)
+    end
+    
+    act_title + ". " + scene_title + speech.first
+  end
 
-  #   speech = act_result.map do |each_hash|
-  #     each_hash.values.flatten
-  #   end.max_by(&:last)
+  def scene_objects
+    scenes = []
+    scene_elements do |scene_element|
+      @scene = Scene.new(scene_element: scene_element)
+      @scene.title
+      @scene.max_scene_line
+      scenes << @scene
+    end
+    title(scenes)
+  end
 
-  #   scene_title = act_result.map { |each_hash| each_hash.key(speech) }.compact[0]
+  def title(scenes)
+    max_speech_value = scenes.map(&:longest_speech).max_by(&:last)
 
-  #   act_title = nil
-  #   result.map do |act|
-  #     act_title = act.title if act.speech.include?(scene_title)
-  #   end
-  #   act_title + ". " + scene_title + speech.first
-  # end
+    result_speech_object = nil
+    scenes.map do |scene|
+      if scene.longest_speech == max_speech_value
+        result_speech_object = scene
+      end
+    end
+    scene_name(result_speech_object)
+  end
 
-  # def scene_objects
-  #   scenes = []
-  #   scene_elements do |scene_element|
-  #     @scene = Scene.new(scene_element: scene_element)
-  #     @scene.title
-  #     @scene.max_scene_line
-  #     scenes << @scene
-  #   end
-  #   title(scenes)
-  # end
-
-  # def title(scenes)
-  #   max_speech_value = scenes.map(&:longest_speech).max_by(&:last)
-
-  #   result_speech_object = nil
-  #   scenes.map do |scene|
-  #     if scene.longest_speech == max_speech_value
-  #       result_speech_object = scene
-  #     end
-  #   end
-  #   scene_name(result_speech_object)
-  # end
-
-  # def scene_name(speech_object)
-  #   speech_object.title + speech_object.longest_speech.first
-  # end
+  def scene_name(speech_object)
+    speech_object.title + speech_object.longest_speech.first
+  end
 
   private
 
