b857f247e6ada631d6bb0f89aacd7ea7b07920a3
viktoria.hrytsiuk <grytsykvika@gmail.com>
2018-12-11 17:14:45 +0200

    change sudoku solver

--- solving-every-sudoku/lib/sudoku/board.rb
+++ solving-every-sudoku/lib/sudoku/board.rb
@@ -1,30 +1,11 @@
 class Board
-  attr_accessor :board
 
-  def initialize
-    @board = []
-  end
-
-  def boards(puzzle_string)
+  def self.boards(puzzle_string)
+    board = []
     puzzle_string.each_line do |line|
       array = line.scan(/\d/).map(&:to_i)
       board << array if array.size == 9
     end
     board
   end
-
-  # def [](x, y)
-  #   @board[y][x]
-  # end
-
-  # def []=(x, y, value)
-  #   @board[y][x] = value
-  # end
-
-  # def row(y)
-  #   Array.new(@board[y])
-  # end
-
-
-
 end
--- solving-every-sudoku/lib/sudoku_solver.rb
+++ solving-every-sudoku/lib/sudoku_solver.rb
@@ -6,7 +6,6 @@ class SudokuSolver
 
   def initialize(puzzle_string)
     @puzzle_string = puzzle_string
-    # @board = board_string
   end
 
   def self.solve(puzzle_string)
@@ -14,7 +13,7 @@ class SudokuSolver
   end
 
   def board
-    @board ||= Board.new.boards(puzzle_string)
+    @board ||= Board.boards(puzzle_string)
   end
 
   def empty_position
@@ -31,49 +30,20 @@ class SudokuSolver
     @empty_positions ||= empty_position
   end
 
-  # def row_valid?
-  #   board.all? do |row|
-  #     new_row = row.reject { |i| i == 0 }
-  #     new_row == new_row.uniq
-  #   end
-  # end
-
-  # def column_valid?
-  #   board.transpose.all? do |row|
-  #     new_row = row.reject { |i| i == 0 }
-  #     new_row == new_row.uniq
-  #   end
-  # end
-
-  def box_array
-    box = []
-    board.each_with_index do |row, row_index|
-      if row_index % 3 == 0
-        row.each_with_index do |col, col_index|
-          if col_index % 3 == 0
-            square = board[row_index][col_index, 3] + board[row_index + 1][col_index, 3] + board[row_index + 2][col_index, 3]
-            box << square
-          end
-        end
-      end
-    end
-    box
-  end
-
-  def check_row(row, number)
+  def row_valid?(row, number)
     board[row].each do |row_num|
       return false if row_num == number
     end
   end
 
-  def check_column(col, number)
+  def column_valid?(col, number)
     column_array = board.transpose
     column_array[col].each do |col_num|
       return false if col_num == number
     end
   end
 
-  def check_box(row, col, number)
+  def box_valid?(row, col, number)
     lower_row = 3 * (row / 3)
     lower_col = 3 * (col / 3)
     upper_row = lower_row + 2
@@ -86,20 +56,11 @@ class SudokuSolver
     end
   end
 
-  def check_value(row, col, number)
-    check_row(row, number) && check_column(col, number) && check_box(row, col, number)
+  def valid_board?(row, col, number)
+    row_valid?(row, number) &&
+      column_valid?(col, number) &&
+        box_valid?(row, col, number)
   end
-
-  # def box_valid?
-  #   box_array.all? do |row|
-  #     new_row = row.reject { |i| i == 0 }
-  #     new_row == new_row.uniq
-  #   end
-  # end
-
-  # def valid_board?
-  #   box_valid? && row_valid? && column_valid?
-  # end
 
   def solve
     pos_index = 0
@@ -111,7 +72,7 @@ class SudokuSolver
       found = false
 
       while !found && possibility <= 9
-        if check_value(row_index, col_index, possibility)
+        if valid_board?(row_index, col_index, possibility)
           found = true
           board[row_index][col_index] = possibility
           pos_index += 1
